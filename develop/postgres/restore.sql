65cce67c172b563a5593475dd92ff848d1badf72
Alexis Mora <alexis.mora1v@gmail.com>
2023-02-22 02:28:18 -0600

    pk added

--- develop/postgres/restore.sql
+++ develop/postgres/restore.sql
@@ -1,4 +1,7 @@
 CREATE TABLE t1 (
-    id INTEGER,
+    actor_id INTEGER,
     a_very_loooooooooong_column_name TEXT NOT NULL DEFAULT ''
 );
+
+ALTER TABLE ONLY public.t1
+    ADD CONSTRAINT actor_pkey PRIMARY KEY (actor_id);
