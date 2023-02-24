4da834501531ce57d3d01877ba2fbda30734fb4e
Alexis Mora <armorav@minsait.com>
2023-02-24 01:30:04 -0600

    test.sql

---
+++ develop/postgres/test.sql
@@ -0,0 +1,7 @@
+IF OBJECT_ID(N'stg.PER_TX_CANALES_CXM_TEN', N'U') IS NOT NULL
+       TRUNCATE TABLE [stg].[PER_TX_CANALES_CXM_TEN];  
+
+CREATE TABLE stg.PER_TX_CANALES_CXM_TEN (
+	ID_BUC int NOT NULL,
+	cxm_mx_det_ten_canalid uniqueidentifier NULL
+);
