8cbba26409a84b78f3e5e87db4f57507ea0bb7f1
Alexis Mora <armorav@minsait.com>
2023-03-01 18:41:39 -0600

    SQL lint error test

--- develop/postgres/test.sql
+++ develop/postgres/test.sql
@@ -4,4 +4,4 @@ IF OBJECT_ID(N'stg.PER_TX_CANALES_CXM_TEN', N'U') IS NOT NULL
 CREATE TABLE stg.PER_TX_CANALES_CXM_TEN (
 	ID_BUC int NOT NULL,
 	cxm_mx_det_ten_canalid uniqueidentifier NULL,
-	);
+);
